389e02bf3844e41cdf833536714235e3c0ea9220
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-01-05 22:50:33 +0700

    CustomBottomNavBar: Adding text with icon button

--- recruitment_management_app/lib/components/custom_bottom_nav_bar.dart
+++ recruitment_management_app/lib/components/custom_bottom_nav_bar.dart
@@ -4,7 +4,7 @@ import 'package:recruitment_management_app/pages/profile/view/profile_view.dart'
 import 'package:recruitment_management_app/pages/calendar/view/calendar_view.dart';
 import '../enums.dart';
 import '../pages/home/view/home_view.dart';
-import 'linear_gradient_mask.dart';
+
 class CustomBottomNavBar extends StatelessWidget {
 
   const CustomBottomNavBar({
@@ -28,45 +28,130 @@ class CustomBottomNavBar extends StatelessWidget {
             color: Color(0xFFDADADA).withOpacity(0.15),
           ),
         ],
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40),
-        ),
+        // borderRadius: BorderRadius.only(
+        //   topLeft: Radius.circular(40),
+        //   topRight: Radius.circular(40),
+        // ),
       ),
       child: SafeArea(
           top: false,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              IconButton(
-                icon: LinearGradientMask(
-                  child: Icon(
-                    Icons.home,
+              Column(
+                mainAxisSize: MainAxisSize.min, // Important
+                children: [
+                  SizedBox(
+                    height: 35,
+                    child: IconButton(
+                      icon: Icon(
+                          Icons.home,
+                          color: selectedMenu == MenuState.home ? kPrimaryColor : inActiveIconColor,
+                      ),
+                      onPressed: () =>
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomePage()),
+                                (Route<dynamic> route) => false,),
+                    ),
                   ),
-                ),
-                onPressed: () =>
-                    Navigator.pushNamed(context, HomePage.routeName),
+                  Text(
+                      "Home",
+                      style: TextStyle(
+                        color: selectedMenu == MenuState.home ? kPrimaryColor : inActiveIconColor,
+                      ),
+                  ),
+                ],
               ),
-              IconButton(
-                icon: Icon(Icons.calendar_today),
-                onPressed: () =>
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => CalendarPage()),
-                            (Route<dynamic> route) => false,),
+              Column(
+                mainAxisSize: MainAxisSize.min, // Important
+                children: [
+                  SizedBox(
+                    height: 35,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.calendar_today,
+                        color: selectedMenu == MenuState.calendar ? kPrimaryColor : inActiveIconColor,
+                      ),
+                      onPressed: () =>
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => CalendarPage()),
+                                (Route<dynamic> route) => false,),
+                    ),
+                  ),
+                  Text(
+                    "Calendar",
+                    style: TextStyle(
+                      color: selectedMenu == MenuState.calendar ? kPrimaryColor : inActiveIconColor,
+                    ),
+                  ),
+                ],
               ),
-              IconButton(
-                icon: Icon(Icons.timer),
-
+              Column(
+                mainAxisSize: MainAxisSize.min, // Important
+                children: [
+                  SizedBox(
+                    height: 35,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.timer,
+                        color: selectedMenu == MenuState.timeOff ? kPrimaryColor : inActiveIconColor,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Time Off",
+                    style: TextStyle(
+                      color: selectedMenu == MenuState.timeOff ? kPrimaryColor : inActiveIconColor,
+                    ),
+                  ),
+                ],
               ),
-              IconButton(
-                icon: Icon(Icons.camera),
-
+              Column(
+                mainAxisSize: MainAxisSize.min, // Important
+                children: [
+                  SizedBox(
+                    height: 35,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.camera,
+                        color: selectedMenu == MenuState.checkIn ? kPrimaryColor : inActiveIconColor,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Check In",
+                    style: TextStyle(
+                      color: selectedMenu == MenuState.checkIn ? kPrimaryColor : inActiveIconColor,
+                    ),
+                  ),
+                ],
               ),
-              IconButton(
-                icon: Icon(Icons.people),
-                onPressed: () =>
-                    Navigator.pushNamed(context, ProfilePage.routeName),
+              Column(
+                mainAxisSize: MainAxisSize.min, // Important
+                children: [
+                  SizedBox(
+                    height: 35,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.people,
+                        color: selectedMenu == MenuState.profile ? kPrimaryColor : inActiveIconColor,
+                      ),
+                      onPressed: () =>
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => ProfilePage()),
+                                (Route<dynamic> route) => false,),
+                    ),
+                  ),
+                  Text(
+                    "Profile",
+                    style: TextStyle(
+                      color: selectedMenu == MenuState.profile ? kPrimaryColor : inActiveIconColor,
+                    ),
+                  ),
+                ],
               ),
             ],
           )),
